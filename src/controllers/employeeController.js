b566962aa92470027d2c477ff1495f0cab98f1a5
Jeyfferson Vicente <jeyffesilv@gmail.com>
2026-09-19 08:10:43 -0300

    criadno as rotas para aplicação

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -1,7 +1,8 @@
 import db from '../config/db.js';
 
 export const getEmployees = (req, res) => {
-  db.query('SELECT * FROM employees', (err, results) => {
+
+  db.query('SELECT * FROM funcionarios', (err, results) => {
     if (err) {
       console.error('Erro ao buscar funcionários:', err);
       res.status(500).json({ error: 'Erro ao buscar funcionários' });
@@ -9,6 +10,20 @@ export const getEmployees = (req, res) => {
     }
     res.json(results);
   });
+  
 };
 
-export const
+export const createEmployee = (req, res) => {
+
+  const { nome, cargo, salario } = req.body;
+
+  const sql = 'INSERT INTO funcionarios (nome, cargo, salario) VALUES (?, ?, ?)';
+ 
+  db.query(sql, [nome, cargo, salario], err => {
+    if (err) return res.status(500).json({ error: 'Erro ao inserir funcionário' });
+    res.json({ message: 'Funcionário inserido com sucesso!' });
+
+  });
+
+}
+
